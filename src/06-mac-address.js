402e33161f21322334af10d61468a76b09d6047e
ElemartA <info88881@gmail.com>
2021-04-11 17:52:18 +0300

    feat: add solution mac-address-tests

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,11 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const res = /^[A-F0-9-]*$/gi.test(n);
+  if (res) {
+    return true;
+  } return false;
 }
 
 module.exports = isMAC48Address;
